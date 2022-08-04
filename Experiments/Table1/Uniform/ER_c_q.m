fbe4b55a151a0778b07d12fad69ee389c645af36
Yeqing Zhou <yeqingzhou92@gmail.com>
2022-08-04 17:04:28 +0200

    Add files via upload

---
+++ Experiments/Table1/Uniform/ER_c_q.m
@@ -0,0 +1,19 @@
+cycle = 100000;
+col = 50;
+r = 50;
+k=2;
+N = 5;   
+c=500; % compute ER ER2 for all c = 10: 500
+ER = zeros(r+1,col);
+ER2 = zeros(r+1,col);
+ERK = zeros(r+1,col);
+ER2K = zeros(r+1,col);
+
+fprintf('Progress:\n');
+fprintf(['\n' repmat('.',1,r+1) '\n\n']);
+
+for i = 1:(r+1)
+        [ER(i,:),ER2(i,:)] = N_opaque((i-1)./100,N,c,cycle);
+        [ERK(i,:),ER2K(i,:)] = k_opaque((i-1)./100,N,c,cycle,k);
+        fprintf('\b|\n');
+end
